9edae89e8a1d945365ebf7c133ff7fee058e872b
eileenzhangyf <zhangbbq@bc.edu>
2022-04-21 19:56:17 -0700

    compose new post activity initialized

--- app/src/main/java/edu/neu/madcourse/paws/ui/post/HomeFragment.java
+++ app/src/main/java/edu/neu/madcourse/paws/ui/post/HomeFragment.java
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.paws.ui.post;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import edu.neu.madcourse.paws.ComposeActivity;
 import edu.neu.madcourse.paws.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -27,13 +31,23 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        FloatingActionButton add_button = binding.floatingActionButton;
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCompose();
+            }
+        });
+
+
+        /*
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 
@@ -41,5 +55,10 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void openCompose() {
+        Intent intent = new Intent(getActivity(), ComposeActivity.class);
+        startActivity(intent);
     }
 }
