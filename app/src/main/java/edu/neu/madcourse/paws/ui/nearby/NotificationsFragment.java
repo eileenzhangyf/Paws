85426e863d89bb1838f2ced60610d24bcb6ac9cf
eileenzhangyf <zhangbbq@bc.edu>
2022-04-24 10:49:54 -0700

    partly merged map activity from Jinxin

--- app/src/main/java/edu/neu/madcourse/paws/ui/nearby/NotificationsFragment.java
+++ app/src/main/java/edu/neu/madcourse/paws/ui/nearby/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.paws.ui.nearby;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import edu.neu.madcourse.paws.MapsActivity;
+import edu.neu.madcourse.paws.R;
 import edu.neu.madcourse.paws.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
@@ -25,6 +30,15 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_notifications,container,false);
+        FloatingActionButton add_button = view.findViewById(R.id.floatingActionButton2);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMaps();
+            }
+        });
+        /*
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
@@ -33,13 +47,18 @@ public class NotificationsFragment extends Fragment {
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
-        return root;
+        });*/
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void openMaps(){
+        Intent intent = new Intent(getActivity(), MapsActivity.class);
+        startActivity(intent);
     }
 }
