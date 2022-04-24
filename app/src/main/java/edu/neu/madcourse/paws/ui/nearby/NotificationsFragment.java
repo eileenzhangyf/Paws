8be9c81a7527bb936e5bbd7a9d10c1824a26d199
eileenzhangyf <zhangbbq@bc.edu>
2022-04-24 12:00:17 -0700

    ask location permission window added

--- app/src/main/java/edu/neu/madcourse/paws/ui/nearby/NotificationsFragment.java
+++ app/src/main/java/edu/neu/madcourse/paws/ui/nearby/NotificationsFragment.java
@@ -1,14 +1,26 @@
 package edu.neu.madcourse.paws.ui.nearby;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -21,8 +33,15 @@ import edu.neu.madcourse.paws.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 99;
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    double longitude;
+    double latitude;
+    public LocationListener locationListener;
+    public LocationManager locationManager;
+    public String provider;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +49,7 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
         View view = inflater.inflate(R.layout.fragment_notifications,container,false);
         FloatingActionButton add_button = view.findViewById(R.id.floatingActionButton2);
         add_button.setOnClickListener(new View.OnClickListener() {
@@ -38,17 +58,13 @@ public class NotificationsFragment extends Fragment {
                 openMaps();
             }
         });
-        /*
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-        return view;
+        locationManager = (LocationManager)  getActivity().getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(),false);
+
+        if(checkLocationPermission())
+            return view;
+        else return root;
     }
 
     @Override
@@ -60,5 +76,52 @@ public class NotificationsFragment extends Fragment {
     public void openMaps(){
         Intent intent = new Intent(getActivity(), MapsActivity.class);
         startActivity(intent);
+    }
+
+    public boolean checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)){
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Allow app to access location?")
+                        .setMessage("This app requires user permission to display location")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(getActivity(),new String[]{
+                                        Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_LOCATION
+                                );
+                            }
+                        })
+                        .create()
+                        .show();
+
+            }else{
+                requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_LOCATION);
+            }
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_LOCATION:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Log.e("location_permission","granted");
+                    Toast.makeText(getActivity().getApplicationContext(),"You are now sharing location",Toast.LENGTH_SHORT).show();
+                    if(ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        locationManager.requestLocationUpdates(provider,400,1, (LocationListener) this);
+                    }
+                }else{
+
+                }
+            }
+        }
+        return;
+
     }
 }
