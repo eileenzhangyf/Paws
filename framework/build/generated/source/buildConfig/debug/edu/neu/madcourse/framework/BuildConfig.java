8e43b5184e010667956eed1550ba65f73b0eeca4
amyxu <67804304+DreamerAmy@users.noreply.github.com>
2022-04-23 19:37:12 -0700

    updated ui

---
+++ framework/build/generated/source/buildConfig/debug/edu/neu/madcourse/framework/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.neu.madcourse.framework;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "edu.neu.madcourse.framework";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final boolean LOG_DEBUG = true;
+}
