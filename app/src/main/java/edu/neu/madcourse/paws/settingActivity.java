fce5937840af5e7f97ec64db4bb91aace1305bf0
eileenzhangyf <zhangbbq@bc.edu>
2022-04-26 19:04:07 -0700

    profile photo feature updated

--- app/src/main/java/edu/neu/madcourse/paws/settingActivity.java
+++ app/src/main/java/edu/neu/madcourse/paws/settingActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -35,11 +37,15 @@ public class settingActivity<onActivityResult> extends AppCompatActivity {
     private ImageView profile_image;
     StorageReference storageReference;
     DatabaseReference user_db;
+    String storage_path ="All_profiles/";
 
     private String curr_user_email;
     FirebaseUser firebaseUser;
     EditText username_input;
     EditText city_input;
+    private int image_request_code = 1000;
+    Uri filepath;
+    long mark;
 
 
     @Override
@@ -53,6 +59,7 @@ public class settingActivity<onActivityResult> extends AppCompatActivity {
         profile_image = (ImageView) findViewById(R.id.profile_setting_image);
         storageReference = FirebaseStorage.getInstance().getReference();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mark = System.currentTimeMillis();
         camera_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,7 +69,7 @@ public class settingActivity<onActivityResult> extends AppCompatActivity {
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                uploadImgtoFirebase();
                 openPetSetting();
             }
         });
@@ -95,53 +102,64 @@ public class settingActivity<onActivityResult> extends AppCompatActivity {
          * open gallery
          */
         Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(openGallery,1000);
+        startActivityForResult(Intent.createChooser(openGallery, "please select image"), image_request_code);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1000){
+        if(requestCode == image_request_code){
             if(resultCode == Activity.RESULT_OK){
-                Uri imageUri = data.getData();
-                Log.e("image_uri",imageUri.toString());
-                profile_image.setImageURI(imageUri);
-                uploadImgtoFirebase(imageUri);
+                filepath = data.getData();
+                Log.e("image_uri",filepath.toString());
+                profile_image.setImageURI(filepath);
+              //  uploadImgtoFirebase(imageUri);
             }
         }
+    }
+    public String GetFileExtension(Uri uri) {
+
+        ContentResolver contentResolver = getContentResolver();
+
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        // Returning the file Extension.
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
     /**
      * upload image to firebase storage
      */
 
-    private void uploadImgtoFirebase(Uri uri) {
-        StorageReference fileReference = storageReference.child("profile.jpg");
-        fileReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        String nickName = username_input.getText().toString();
-                        String cityName = city_input.getText().toString();
+    private void uploadImgtoFirebase() {
+        if (filepath != null) {
+            StorageReference fileReference2nd = storageReference.child(storage_path +mark+"."+ GetFileExtension(filepath));
+            fileReference2nd.putFile(filepath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileReference2nd.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String nickName = username_input.getText().toString();
+                            String cityName = city_input.getText().toString();
 
-                        String upload_id = user_db.push().getKey();
+                            String upload_id = user_db.push().getKey();
 
-                        User user = new User(curr_user_email,nickName,uri.toString(),cityName);
-                        user_db.child(upload_id).setValue(user);
-                    }
-                });
-                Toast.makeText(getApplicationContext(),"image uploaded",Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(),"failed",Toast.LENGTH_SHORT).show();
-                Log.e("upload_reason",e.toString());
-            }
-        });
+                            User user = new User(curr_user_email, nickName, uri.toString(), cityName);
+                            user_db.child(upload_id).setValue(user);
+                        }
+                    });
+                    Toast.makeText(getApplicationContext(), "image uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+                    Log.e("upload_reason", e.toString());
+                }
+            });
 
+        }
     }
 }
