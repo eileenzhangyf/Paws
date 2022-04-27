7aa0fbd15d87c98e4ead509d832c8e436ba62979
Jinxin Hu <hujinxin0607@gmail.com>
2022-04-26 22:57:01 -0700

    build json reader for profile API

--- app/src/main/java/edu/neu/madcourse/paws/Pet.java
+++ app/src/main/java/edu/neu/madcourse/paws/Pet.java
@@ -38,4 +38,24 @@ public class Pet {
     public String getGender(){
         return this.gender;
     }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public void setPet_type(String pet_type) {
+        this.pet_type = pet_type;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
--- app/src/main/java/edu/neu/madcourse/paws/ui/profile/ProfileFragment.java
+++ app/src/main/java/edu/neu/madcourse/paws/ui/profile/ProfileFragment.java
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import edu.neu.madcourse.paws.DownloadUrl;
 import edu.neu.madcourse.paws.ImageUploadInfo;
+import edu.neu.madcourse.paws.Pet;
 import edu.neu.madcourse.paws.R;
 import edu.neu.madcourse.paws.databinding.FragmentProfileBinding;
 
@@ -28,7 +30,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
@@ -51,15 +61,45 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        String url = "https://paws-846aa-default-rtdb.firebaseio.com/pet_info.json";
+        getDataFromURL(url);
+
+
         return root;
-
     }
 
-
-
-
-
-
-
-
+    private String getDataFromURL(String url) {
+        DownloadUrl downloadUrl = new DownloadUrl();
+        String allData = "";
+        try {
+            allData = downloadUrl.retirveveUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allData;
     }
+
+    private void cleanDataFromURL(String s) {
+        HashMap<String,Pet> pets = new HashMap<>();
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            Iterator iterator = jsonObject.keys();
+
+            while (iterator.hasNext()) {
+                String key = (String) iterator.next();
+                JSONObject value = (JSONObject) jsonObject.get(key);
+                String age = value.getString("age");
+                String breed = value.getString("breed");
+                String gender = value.getString("gender");
+                String pet_type = value.getString("pet_type");
+                String user_name = value.getString("user_name");
+                pets.put(user_name, new Pet(user_name,pet_type,age, breed,gender));
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
